8123546597e2ea114c3c1b800ef09ae38f566d4a
paidaxing <32479732@qq.com>
2023-05-04 22:52:00 +0800

    [paidaxing] #N/A feat: add log

--- channel/wechat/wechat_mp_channel.py
+++ channel/wechat/wechat_mp_channel.py
@@ -76,7 +76,8 @@ class WechatSubsribeAccount(Channel):
         robot.run()
 
     def handle(self, msg, count=1):
-        logger.info("msg:{}", str(msg))
+        logger.info("msg.source:{}", str(msg.source))
+        logger.info("msg.content:{}", str(msg.content))
         if msg.content == "继续":
             return self.get_un_send_content(msg.source)
 
